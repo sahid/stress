8a39a5fca2777254731dc03d6accbc1c9f715098
Sahid Orentino Ferdjaoui <sahid.ferdjaoui@cloudwatt.com>
2013-10-06 10:21:02 +0200

    Removes cluster for client

--- clt.js
+++ clt.js
@@ -1,6 +1,5 @@
 var program = require('commander');
 var io = require('socket.io-client')
-var cluster = require('cluster');
 
 var DEFAULT_MAX_CONNECTIONS = 100;
 var DEFAULT_BUCKET_SIZE = 10;
@@ -16,40 +15,31 @@ var gl_accepted = 0;
 var gl_failed = 0;
 var msg_received = 0;
 
-var flood = function(host, service, retryfailed) {
+var flood = function(host, service) {
     var socket = io.connect("http://"+host+":"+service+"/", 
-			    {'force new connection': true,
-                             'close timeout': DEFAULT_CLOSE_TIMEOUT,
-                             'reconnection delay': 500,
-                             'reconnection limit': 32000,
-                             'max reconnection attempts': 10});
+			    {'force new connection': true,});
     socket.on("connect", function() {
 	gl_accepted += 1
-	/*
-	socket.emit("client");
 	socket.on("stream", function(data) {
-	    //console.log(data);
-	    msg_received ++;
+	    // TODO(sahid): Needs to implement.
 	});  
-	*/
-   });
-
+    });
+    
     socket.on('connect_failed', function(){
 	gl_failed += 1;
     });
-
+    
     socket.on('disconnect', function() {
 	gl_failed += 1;
     });  
 };
 
-var loop = function(conn, bsize, bms, host, service, retryfailed) {
+var loop = function(conn, bsize, bms, host, service) {
     var idint = setInterval(function() {
 	for (var i=0; i<bsize; i++) {
 	    if (gl_spawned >= conn) {    
-		//clearInterval(idint);
 	    } else {
-		flood(host, service, retryfailed);
+		flood(host, service);
 		gl_spawned += 1;
 	    }
 	}
@@ -61,8 +51,6 @@ var loop = function(conn, bsize, bms, host, service, retryfailed) {
 	.version('0.1')
 	.option('-c, --connections <n>', 
 		'Number of connections', parseInt)
-	.option('-r, --retry-failed', 
-		'Retry connection failed because of pool timeout.')
 	.option('-b, --bucket-size <n>', 
 		'Number of connection launched every "bucket-ms"', parseInt)
 	.option('-m, --bucket-ms <n>', 
@@ -78,29 +66,14 @@ var loop = function(conn, bsize, bms, host, service, retryfailed) {
     var conn = program.connections || DEFAULT_MAX_CONNECTIONS;
     var bsize = program.bucketSize || DEFAULT_BUCKET_SIZE;
     var bms = program.bucketMs || DEFAULT_BUCKET_MS;
-    var retryfailed = program.retryFailed || DEFAULT_RETRY_FAILED;
     var service = program.port || DEFAULT_SERVICE;
     var host = program.host || DEFAULT_HOST;
-    var nproc = program.numProc || DEFAULT_NUMPROCESS;
+    
+    setInterval(function() {
+	console.log("Connections spawned: " + gl_spawned + 
+		    ", Connections accepted: "+ gl_accepted +
+		    ", Connections failed: " + gl_failed + ".\r");
+    }, 1000);
 
-    var conn_per_clt = conn / nproc;
-
-    if (cluster.isMaster) {
-	// Fork workers.
-	for (var i = 0; i < nproc; i++) {
-	    cluster.fork();
-	}
-	cluster.on('death', function(worker) {
-	    console.log('worker ' + worker.pid + ' died');
-	});
-    } else {
-	// workers
-	loop(conn_per_clt, bsize, bms, host, service, retryfailed);
-	setInterval(function() {
-	    console.log("Worker:" + cluster.worker.id + " Connections spawned: " + gl_spawned + 
-			", Connections accepted: "+ gl_accepted +
-			", Connections failed: " + gl_failed + ".\r");
-	}, 1000);
-
-    }
+    loop(conn, bsize, bms, host, service);
 })();
