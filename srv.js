284f5bcc5b1c944a72a9d6c07ef89a0ba6d65a4f
Sahid Orentino Ferdjaoui <sahid.ferdjaoui@cloudwatt.com>
2013-10-06 10:07:24 +0200

    Review + adds cluster solution

--- srv.js
+++ srv.js
@@ -1,55 +1,49 @@
 var program = require('commander');
-var os = require('os');
 var cluster = require('cluster');
+var os = require('os');
 
-var DEFAULT_HOST = "127.0.0.1";
-var DEFAULT_SERVICE = 8080;
-var DEFAULT_CHUNK_MS = 10000; //10s
-var DEBUG = !true;
 var MB = 1024*1024;
+var DEBUG = !true;
 
-var dispatch = function(store, host, service, chunkms, cpus) {
-    var io = require('socket.io').listen(service, {log: DEBUG
-						   "close timeout": 10000,
-                                                   "heartbeat timeout": 5000,
-                                                   "heartbeat interval": 15000,
-						  }); //,
-    io.configure(function() {
-	io.set('store', store);
-    });
-    io.sockets.on('connection', function (socket) {
-	//console.info("Stress's server welcomes to you. Your id: " + socket.id);
-	/*
-	socket.on("agent", function(data) {
-	    socket.emit("response", {
-		clients: io.sockets.clients().length,
-		loadavg: os.loadavg(),
-		totalmem: os.totalmem(),
-		freemem: os.freemem(),
-		cpus:os.cpus(),
-		cpusused:cpus
-	    });
-	});
-	*/
+var DEFAULT_SERVICE = 8080;
+var DEFAULT_CHUNK_MS = 10000;
+
+var serv = function(service, chunkms, cpus) {
+    var io = require('socket.io').listen(service, {
+	"log": DEBUG, 
+	"heatbeat":  false,});
+
+    if (cpus > 1) {
+	var RedisStore = require('socket.io/lib/stores/redis')
+	, redis  = require('socket.io/node_modules/redis')
+	, pub    = redis.createClient()
+	, sub    = redis.createClient()
+	, client = redis.createClient();
 	
-	// Event client, it registers a new client.
-	socket.on("client", function(data) {
-	    (feed = function(n) {
-		socket.emit("stream", {id: socket.id, chunk: n});
-		setTimeout(function() {
-		    feed(n+1);
-		}, chunkms);
-	    })(0);
-	});
-    });
+	io.set('store', new RedisStore({
+	    redisPub : pub
+	    , redisSub : sub
+	    , redisClient : client
+	}));
+    }
 
+    io.sockets.on('connection', function (socket) {
+	(feed = function(n) {
+	    socket.emit("stream", {id: socket.id, chunk: n});
+	    setTimeout(function() {
+		feed(n+1);
+	    }, chunkms);
+	})(0);
+    });
+    
     if (cluster.worker.id == 1) {
 	setInterval(function() {
 	    console.log("\033[2J");
 	    console.log("clients: " + io.sockets.clients().length);
 	    console.log("total mem(mb): " + os.totalmem()/MB);
 	    console.log("free mem(mb): " + os.freemem()/MB);
-	    console.log("load avg: " + os.loadavg()[0])
+	    console.log("cpus used: " + cpus);
+	    console.log("load avg: " + os.loadavg()[0]);
 	}, 1000);
     }
 };
@@ -62,29 +56,19 @@ var dispatch = function(store, host, service, chunkms, cpus) {
 		'The time between each reponse. default:1000', parseInt)
 	.option('-p, --port <n>', 
 		'The service this server handler. default:8080', parseInt)
-	.option('-h, --host <n>', 
-		'The service this server handler. default:127.0.0.1')
 	.option('-n, --num-cpus <n>', 
 		'Set the number of cpu cores used. default:use system')
 	.parse(process.argv);
     
     var chunkms = program.connections || DEFAULT_CHUNK_MS;
     var service = program.port || DEFAULT_SERVICE;
-    var host = program.host || DEFAULT_HOST;
     var cpus = program.numCpus || os.cpus().length;
-
-    var store = new (require('socket.io-clusterhub'));    
+    
     if (cluster.isMaster) {
-	// Fork workers.
 	for (var i = 0; i < cpus; i++) {
 	    cluster.fork();
 	}
-	
-	cluster.on('death', function(worker) {
-	    console.log('worker ' + worker.pid + ' died');
-	});
     } else {
-	// workers
-	dispatch(store, host, service, chunkms, cpus);
+	serv(service, chunkms, cpus);
     }
 })();
